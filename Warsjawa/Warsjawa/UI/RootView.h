402c80322d44541f5da39c9f33d5a161f26bb672
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2014-09-27 00:33:09 +0200

    protocol (delegate) added;

--- Warsjawa/Warsjawa/UI/RootView.h
+++ Warsjawa/Warsjawa/UI/RootView.h
@@ -1,7 +1,14 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@class RootView;
+
+@protocol RootViewDelegate
+- (void)rootViewDidRequestChangeBackgroundColor:(RootView *)rootView;
+@end
+
 @interface RootView : UIView
+@property (nonatomic, weak, readwrite) id<RootViewDelegate> delegate;
 @property (nonatomic, strong, readonly) UILabel *changesCounterLabel;
 @property (nonatomic, strong, readonly) UIButton *changeBackgroundColorButton;
 @end
